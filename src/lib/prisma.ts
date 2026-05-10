4081ef3cfe6fe092cc777504c77aa6a05a8349c5
banothsrikanth <banothsrikanth015@gmail.com>
2026-05-10 22:52:20 +0530

    fix prisma client generation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,42 @@
 
-import { PrismaClient } from '../generated/prisma/client';
+// import { PrismaClient } from '../generated/prisma/client';
+// declare global {
+//     namespace globalThis {
+//         var prismadb: PrismaClient;
+//     }
+// }
+
+// const prisma = globalThis.prismadb || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismadb = prisma;
+
+// export default prisma;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { PrismaClient } from "@prisma/client";
+
 declare global {
-    namespace globalThis {
-        var prismadb: PrismaClient;
-    }
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prismadb || new PrismaClient();
+const prisma =
+  global.prisma ||
+  new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prismadb = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
